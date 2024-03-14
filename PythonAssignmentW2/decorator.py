018e2f5e39ed047947cb7068d91ed43ff8bb9359
shvnsh7 <shvnsh7@gmail.com>
2024-03-14 11:34:54 +0530

    report

---
+++ PythonAssignmentW2/decorator.py
@@ -0,0 +1,14 @@
+"""importing necessary module time """
+import time
+
+def log_execution_time(func):
+    """log exceution"""
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        execution_time = end_time - start_time
+        with open("execution_logs.txt", "a",encoding="utf-8") as f:
+            f.write(f"Function '{func.__name__}' executed in {execution_time:.4f} seconds\n")
+        return result
+    return wrapper
